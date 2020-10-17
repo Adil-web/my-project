9dd6b8fd830744b0c50f2e4ea00007ff02dc11d9
unknown <blaze.poduction@mail.ru>
2020-10-17 18:10:42 +0600

    test

---
+++ precache-manifest.c453d4dfd37ad91b7417e78851574052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4472149754d02b29c59b",
+    "url": "/my-project/css/app.bbf8d734.css"
+  },
+  {
+    "revision": "41bcae08533451009ad1",
+    "url": "/my-project/css/chunk-vendors.46138cb2.css"
+  },
+  {
+    "revision": "2c8ae5154ba225d23a1c0b5a2f19e92f",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "4472149754d02b29c59b",
+    "url": "/my-project/js/app.d490b736.js"
+  },
+  {
+    "revision": "41bcae08533451009ad1",
+    "url": "/my-project/js/chunk-vendors.731e8ea5.js"
+  },
+  {
+    "revision": "6808c8c0115be2ee3d3e729a4e3ff79a",
+    "url": "/my-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-project/robots.txt"
+  }
+]);
